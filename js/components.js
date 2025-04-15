b315f4737bfa0a8bc11f989d01b27e6e951c4b27
Tân Dv <tandv@192.168.1.6.non-exists.ptr.local>
2025-04-15 21:41:01 +0700

    update

--- js/components.js
+++ js/components.js
@@ -1,7 +1,56 @@
+defineComponent('IconArrowLeft', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" class="w-6 h-6">
+            <path fill="currentColor" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
+        </svg>
+    `
+});
+
+defineComponent('IconArrowRight', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" class="w-6 h-6">
+            <path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z"/>
+        </svg>
+    `
+});
+
+defineComponent('IconEyeVisible', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="w-5 h-5">
+            <circle cx="12" cy="12" r="1.5" fill="currentColor"/>
+            <path fill="currentColor" d="M15.29 18.12L14 16.78l-.07-.07l-1.27-1.27a4 4 0 0 1-.61.06A3.5 3.5 0 0 1 8.5 12a4 4 0 0 1 .06-.61l-2-2L5 7.87a15.9 15.9 0 0 0-2.87 3.63a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25a9.5 9.5 0 0 0 3.23-.67ZM8.59 5.76l2.8 2.8A4 4 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a4 4 0 0 1-.06.61l2.68 2.68l.84.84a15.9 15.9 0 0 0 2.91-3.63a1 1 0 0 0 0-1c-.64-1.11-4.16-6.68-10.14-6.5a9.5 9.5 0 0 0-3.23.67Zm12.12 13.53L19.41 18l-2-2l-9.52-9.53L6.42 5L4.71 3.29a1 1 0 0 0-1.42 1.42L5.53 7l1.75 1.7l7.31 7.3l.07.07L16 17.41l.59.59l2.7 2.71a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42"/>
+        </svg>
+    `
+});
+
+defineComponent('IconEyeHidden', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="w-5 h-5">
+            <circle cx="12" cy="12" r="1.5" fill="currentColor"/>
+            <path fill="currentColor" d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5c-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1m-9.87 4a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5"/>
+        </svg>
+    `
+});
+
+defineComponent('IconSearch', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path fill="#9ca3af" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/>
+        </svg>
+    `
+});
+
+defineComponent('IconFilter', {
+    template: `
+        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+            <path fill="none" stroke="#9ca3af" stroke-width="1.5" d="M19 3H5c-1.414 0-2.121 0-2.56.412S2 4.488 2 5.815v.69c0 1.037 0 1.556.26 1.986s.733.698 1.682 1.232l2.913 1.64c.636.358.955.537 1.183.735c.474.411.766.895.898 1.49c.064.284.064.618.064 1.285v2.67c0 .909 0 1.364.252 1.718c.252.355.7.53 1.594.88c1.879.734 2.818 1.101 3.486.683S15 19.452 15 17.542v-2.67c0-.666 0-1 .064-1.285a2.68 2.68 0 0 1 .899-1.49c.227-.197.546-.376 1.182-.735l2.913-1.64c.948-.533 1.423-.8 1.682-1.23c.26-.43.26-.95.26-1.988v-.69c0-1.326 0-1.99-.44-2.402C21.122 3 20.415 3 19 3Z"/>
+        </svg>
+    `
+});
+
 defineComponent('Card', {
     template: `
         <div class="p-5">
-            <!-- Credit Card -->
             <div class="relative mx-auto" style="width: calc(100% - 60px);">
                 <div class="relative h-36">
                     <div v-if="currentCard" 
@@ -10,62 +59,33 @@ defineComponent('Card', {
                          :class="currentCard.gradient">
                     </div>
                 </div>
-                
-                <!-- Previous Button -->
                 <button v-show="showPrevButton"
                         @click="previousCard" 
                         class="absolute top-1/2 -left-10 -translate-y-1/2 w-10 h-10 flex items-center justify-center text-gray-400 hover:text-gray-600 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" class="w-6 h-6">
-                        <path fill="currentColor" d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"/>
-                    </svg>
+                    <IconArrowLeft />
                 </button>
-                
-                <!-- Next Button -->
                 <button v-show="showNextButton"
                         @click="nextCard"
                         class="absolute top-1/2 -right-10 -translate-y-1/2 w-10 h-10 flex items-center justify-center text-gray-400 hover:text-gray-600 rounded-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" class="w-6 h-6">
-                        <path fill="currentColor" d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z"/>
-                    </svg>
+                    <IconArrowRight />
                 </button>
             </div>
-
-            <!-- Card Information -->
             <div class="relative" style="height: 84px">
                 <div v-if="currentCard" 
                      :key="currentCard.cardId"
                      class="bg-white rounded-xl -mt-10 shadow-md overflow-hidden absolute inset-x-0">
-                    <!-- Card Number -->
                     <div class="p-3">
                         <div class="flex justify-between items-center">
                             <span class="text-gray-600 text-sm">Card number</span>
                             <div class="flex items-center gap-2">
                                 <span class="font-medium transition-opacity duration-300 opacity-100">{{currentCard.cardNumber}}</span>
                                 <button @click="toggleVisibility" class="text-gray-600 hover:text-gray-800">
-                                    <svg v-if="isVisible" 
-                                        xmlns="http://www.w3.org/2000/svg" 
-                                        width="24" 
-                                        height="24" 
-                                        viewBox="0 0 24 24"
-                                        class="w-5 h-5">
-                                        <circle cx="12" cy="12" r="1.5" fill="currentColor"/>
-                                        <path fill="currentColor" d="M15.29 18.12L14 16.78l-.07-.07l-1.27-1.27a4 4 0 0 1-.61.06A3.5 3.5 0 0 1 8.5 12a4 4 0 0 1 .06-.61l-2-2L5 7.87a15.9 15.9 0 0 0-2.87 3.63a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25a9.5 9.5 0 0 0 3.23-.67ZM8.59 5.76l2.8 2.8A4 4 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5a4 4 0 0 1-.06.61l2.68 2.68l.84.84a15.9 15.9 0 0 0 2.91-3.63a1 1 0 0 0 0-1c-.64-1.11-4.16-6.68-10.14-6.5a9.5 9.5 0 0 0-3.23.67Zm12.12 13.53L19.41 18l-2-2l-9.52-9.53L6.42 5L4.71 3.29a1 1 0 0 0-1.42 1.42L5.53 7l1.75 1.7l7.31 7.3l.07.07L16 17.41l.59.59l2.7 2.71a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42"/>
-                                    </svg>
-                                    <svg v-else  
-                                        xmlns="http://www.w3.org/2000/svg" 
-                                        width="24" 
-                                        height="24" 
-                                        viewBox="0 0 24 24"
-                                        class="w-5 h-5">
-                                        <circle cx="12" cy="12" r="1.5" fill="currentColor"/>
-                                        <path fill="currentColor" d="M21.87 11.5c-.64-1.11-4.16-6.68-10.14-6.5c-5.53.14-8.73 5-9.6 6.5a1 1 0 0 0 0 1c.63 1.09 4 6.5 9.89 6.5h.25c5.53-.14 8.74-5 9.6-6.5a1 1 0 0 0 0-1m-9.87 4a3.5 3.5 0 1 1 3.5-3.5a3.5 3.5 0 0 1-3.5 3.5"/>
-                                    </svg>
+                                    <IconEyeVisible v-if="isVisible" />
+                                    <IconEyeHidden v-else />
                                 </button>
                             </div>
                         </div>
                     </div>
-
-                    <!-- Card Type Section with Background -->
                     <div :class="currentCard.bgColor" class="text-white p-3 rounded-b-xl">
                         <div class="flex justify-between items-center">
                             <span class="text-sm opacity-90">Card type</span>
@@ -75,8 +95,6 @@ defineComponent('Card', {
                     </div>
                 </div>
             </div>
-
-            <!-- Navigation Dots -->
             <div class="flex justify-center gap-2 mt-5">
                 <span v-for="(card, index) in cards" 
                       :key="card.cardId"
@@ -89,126 +107,60 @@ defineComponent('Card', {
             </div>
         </div>
     `,
-
     data() {
         return {
             cards: [
-                {
-                    cardId: 'card1',
-                    cardNumber: '407459******9601',
-                    unmaskedNumber: '4074591234569601',
-                    cardType: 'Woori Visa Platinum Credit',
-                    subType: 'VV Lux Point Card',
-                    gradient: 'from-gray-700 to-gray-800',
-                    bgColor: 'bg-blue-500'
-                },
-                {
-                    cardId: 'card2',
-                    cardNumber: '512378******4321',
-                    unmaskedNumber: '5123781234564321',
-                    cardType: 'Shinhan MasterCard Gold',
-                    subType: 'Premium Rewards',
-                    gradient: 'from-yellow-600 to-yellow-700',
-                    bgColor: 'bg-yellow-600'
-                },
-                {
-                    cardId: 'card3',
-                    cardNumber: '356012******7890',
-                    unmaskedNumber: '3560121234567890',
-                    cardType: 'KB JCB Signature',
-                    subType: 'Travel Miles',
-                    gradient: 'from-green-600 to-green-700',
-                    bgColor: 'bg-green-600'
-                },
-                {
-                    cardId: 'card4',
-                    cardNumber: '622123******5678',
-                    unmaskedNumber: '6221231234565678',
-                    cardType: 'Hana UnionPay Diamond',
-                    subType: 'Shopping & Dining',
-                    gradient: 'from-purple-600 to-purple-700',
-                    bgColor: 'bg-purple-600'
-                }
+                { cardId: 'card1', cardNumber: '407459******9601', unmaskedNumber: '4074591234569601', cardType: 'Woori Visa Platinum Credit', subType: 'VV Lux Point Card', gradient: 'from-gray-700 to-gray-800', bgColor: 'bg-blue-500' },
+                { cardId: 'card2', cardNumber: '512378******4321', unmaskedNumber: '5123781234564321', cardType: 'Shinhan MasterCard Gold', subType: 'Premium Rewards', gradient: 'from-yellow-600 to-yellow-700', bgColor: 'bg-yellow-600' },
+                { cardId: 'card3', cardNumber: '356012******7890', unmaskedNumber: '3560121234567890', cardType: 'KB JCB Signature', subType: 'Travel Miles', gradient: 'from-green-600 to-green-700', bgColor: 'bg-green-600' },
+                { cardId: 'card4', cardNumber: '622123******5678', unmaskedNumber: '6221231234565678', cardType: 'Hana UnionPay Diamond', subType: 'Shopping & Dining', gradient: 'from-purple-600 to-purple-700', bgColor: 'bg-purple-600' }
             ],
-            
-            // UI State
             isVisible: false,
             activeIndex: 0
-        }
+        };
     },
-
     computed: {
-        /**
-         * Get the current card based on activeIndex
-         */
         currentCard() {
             return this.cards[this.activeIndex];
         },
-        
         showPrevButton() {
             return this.activeIndex > 0;
         },
-        
         showNextButton() {
             return this.activeIndex < this.cards.length - 1;
         }
     },
-
     methods: {
-        /**
-         * Toggle card number visibility
-         * Switches between masked and unmasked number
-         */
         toggleVisibility() {
             this.isVisible = !this.isVisible;
-            // Update only the current card's number
             this.cards[this.activeIndex].cardNumber = this.isVisible
-                ? this.currentCard.unmaskedNumber  // Unmasked number
-                : this.currentCard.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');  // Masked number
+                ? this.currentCard.unmaskedNumber
+                : this.currentCard.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');
         },
-        
-        /**
-         * Navigate to the next card
-         */
         nextCard() {
             this.isVisible = false;
             this.activeIndex = (this.activeIndex + 1) % this.cards.length;
-            
-            // Reset card number to masked version
-            this.cards.forEach((card, index) => {
-                card.cardNumber = card.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');
-            });
+            this.resetCardNumbers();
         },
-        
-        /**
-         * Navigate to the previous card
-         */
         previousCard() {
             this.isVisible = false;
             this.activeIndex = (this.activeIndex - 1 + this.cards.length) % this.cards.length;
-            
-            // Reset card number to masked version
-            this.cards.forEach((card, index) => {
-                card.cardNumber = card.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');
-            });
+            this.resetCardNumbers();
         },
-        
-        /**
-         * Go to a specific card by index
-         */
         goToCard(index) {
             if (index >= 0 && index < this.cards.length) {
                 this.isVisible = false;
                 this.activeIndex = index;
-                
-                // Reset card number to masked version
-                this.cards.forEach((card, idx) => {
-                    card.cardNumber = card.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');
-                });
+                this.resetCardNumbers();
             }
+        },
+        resetCardNumbers() {
+            this.cards.forEach(card => {
+                card.cardNumber = card.unmaskedNumber.replace(/\d{6}(?=\d{4}$)/, '******');
+            });
         }
     }
-})
+});
 
 defineComponent('SubCardMenu', {
     props: {
@@ -271,15 +223,10 @@ defineComponent('CardPromotion', {
     props: {
         title: String,
         promotions: Array,
-        onViewAll: Function
     },
     methods: {
         handleViewAll() {
-            if (this.onViewAll) {
-                this.onViewAll(); // Call the provided function
-            } else {
-                this.$emit('view-all'); // Emit a "view-all" event if no function is provided
-            }
+            this.$router.push('/promotion'); // Navigate to the /promotion page
         }
     },
     template: `
@@ -288,13 +235,13 @@ defineComponent('CardPromotion', {
                 <h2 class="text-sm font-medium text-gray-800">{{ title }}</h2>
                 <a href="#" class="text-sm text-blue-600" @click.prevent="handleViewAll">View all</a>
             </div>
-            <div class="flex gap-4 justify-between overflow-x-auto mt-3 pl-5 scrollbar-hide"
+            <div class="flex gap-4 justify-between overflow-x-auto mt-3 px-5 py-2 scrollbar-hide"
                  style="-ms-overflow-style: none; scrollbar-width: none;">
                 <CardPromotionItem 
                     v-for="(promotion, index) in promotions" 
                     :key="index" 
                     :title="promotion.title" 
-                    :class="index === promotions.length - 1 ? 'mr-5' : ''" />
+                />
             </div>
         </div>
     `
@@ -335,10 +282,9 @@ defineComponent('PromotionItemDetail', {
         }
     },
     template: `
-        <div class="flex items-center bg-white shadow-md overflow-hidden p-3 mb-4" style="border-radius: 12px;">
+        <div class="flex items-center bg-white shadow-md overflow-hidden p-3 mb-4 rounded-lg">
             <div 
-                class="w-[140px] h-20 bg-gray-200 flex items-center justify-center" 
-                :style="{ borderRadius: imageBorderRadius + 'px' }"
+                class="w-[140px] h-20 bg-gray-200 flex items-center justify-center rounded-lg"
             >
                 <span class="text-xs text-gray-500">Image</span>
             </div>
@@ -438,7 +384,7 @@ defineComponent('FilterPopup', {
                                         @change="toggleSelection(product, selectedCardProducts)"
                                     >
                                     <div 
-                                        class="w-[18px] h-[18px] rounded-[4px] flex items-center justify-center"
+                                        class="w-[18px] h-[18px] rounded-lg flex items-center justify-center"
                                         :class="isChecked(product, selectedCardProducts) ? 'bg-blue-500' : 'bg-white border border-solid border-gray-400'"
                                     >
                                         <svg 
--- js/pages.js
+++ js/pages.js
@@ -13,16 +13,6 @@ definePage('HomePage', {
             ]
         };
     },
-    methods: {
-        handleViewAllPromotions() {
-            console.log('View all promotions clicked');
-            // Navigate to PromotionPage or handle logic
-        },
-        handleViewAllWooriStore() {
-            console.log('View all Woori Store promotions clicked');
-            // Navigate to PromotionPage or handle logic
-        }
-    },
     template: `
         <div>
             <Card />
@@ -30,11 +20,11 @@ definePage('HomePage', {
             <CardPromotion 
                 title="Chương trình ưu đãi" 
                 :promotions="promotions1" 
-                :onViewAll="handleViewAllPromotions" />
+            />
             <CardPromotion 
                 title="Ưu đãi Woori store" 
                 :promotions="promotions2" 
-                :onViewAll="handleViewAllWooriStore" />
+            />
         </div>
     `
 });
@@ -43,33 +33,13 @@ definePage('PromotionPage', {
     data() {
         return {
             searchQuery: '',
-            showFilterPopup: false, // State to control the visibility of the filter popup
+            showFilterPopup: false,
             promotions: [
-                {
-                    image: '', // Placeholder for now
-                    title: 'Premium service gift for Woori VV Lux Point cardholders 2025',
-                    linkText: 'Read more'
-                },
-                {
-                    image: '', // Placeholder for now
-                    title: 'Golf Gift Voucher reward for Woori Platinum cardholders',
-                    linkText: 'Read more'
-                },
-                {
-                    image: '', // Placeholder for now
-                    title: 'Swipe Woori cards, get new year\'s lucky money',
-                    linkText: 'Read more'
-                },
-                {
-                    image: '', // Placeholder for now
-                    title: 'Cherish your birthday with Woori VV Card',
-                    linkText: 'Read more'
-                },
-                {
-                    image: '', // Placeholder for now
-                    title: '0% interest installment payment at partner stores',
-                    linkText: 'Read more'
-                }
+                { image: '', title: 'Premium service gift for Woori VV Lux Point cardholders 2025', linkText: 'Read more' },
+                { image: '', title: 'Golf Gift Voucher reward for Woori Platinum cardholders', linkText: 'Read more' },
+                { image: '', title: 'Swipe Woori cards, get new year\'s lucky money', linkText: 'Read more' },
+                { image: '', title: 'Cherish your birthday with Woori VV Card', linkText: 'Read more' },
+                { image: '', title: '0% interest installment payment at partner stores', linkText: 'Read more' }
             ]
         };
     },
@@ -83,19 +53,19 @@ definePage('PromotionPage', {
     methods: {
         handleReadMore(title) {
             console.log(`Read more clicked for: ${title}`);
-            // Add logic to navigate to the promotion detail page or handle the event
+            this.$router.push('/promotion/detail');
         },
         toggleFilterPopup() {
-            this.showFilterPopup = !this.showFilterPopup; // Toggle the popup visibility
+            this.showFilterPopup = !this.showFilterPopup;
         }
     },
     template: `
         <div class="bg-gray-100 min-h-screen">
             <div class="bg-blue-500 mb-4">
-                <div class="flex justify-between items-center px-4 py-8">
-                    <div class="flex items-center bg-white shadow-md px-4 py-2 w-full max-w-md" style="border-radius: 12px;">
+                <div class="flex justify-between items-center px-4 py-8 shadow-custom">
+                    <div class="flex items-center bg-white shadow-custom px-4 py-2 w-full max-w-md rounded-lg">
                         <span class="w-6 h-6 rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="#9ca3af" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/></svg>
+                            <IconSearch />
                         </span>
                         <input 
                             type="text" 
@@ -105,12 +75,11 @@ definePage('PromotionPage', {
                         />
                     </div>
                     <button 
-                        class="flex items-center bg-white shadow-md px-6 py-2 ml-4" 
-                        style="border-radius: 12px;" 
+                        class="flex items-center bg-white shadow-custom px-6 py-2 ml-4 rounded-lg" 
                         @click="toggleFilterPopup"
                     >
                         <span class="w-6 h-6 rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="none" stroke="#9ca3af" stroke-width="1.5" d="M19 3H5c-1.414 0-2.121 0-2.56.412S2 4.488 2 5.815v.69c0 1.037 0 1.556.26 1.986s.733.698 1.682 1.232l2.913 1.64c.636.358.955.537 1.183.735c.474.411.766.895.898 1.49c.064.284.064.618.064 1.285v2.67c0 .909 0 1.364.252 1.718c.252.355.7.53 1.594.88c1.879.734 2.818 1.101 3.486.683S15 19.452 15 17.542v-2.67c0-.666 0-1 .064-1.285a2.68 2.68 0 0 1 .899-1.49c.227-.197.546-.376 1.182-.735l2.913-1.64c.948-.533 1.423-.8 1.682-1.23c.26-.43.26-.95.26-1.988v-.69c0-1.326 0-1.99-.44-2.402C21.122 3 20.415 3 19 3Z"/></svg>
+                            <IconFilter />
                         </span>
                         <span class="ml-2 text-sm text-gray-400">Filter</span>
                     </button>
@@ -124,7 +93,7 @@ definePage('PromotionPage', {
                     :title="promotion.title" 
                     :linkText="promotion.linkText" 
                     @read-more="handleReadMore"
-                    style="border-radius: 12px;" 
+                    class="shadow-custom"
                     :imageBorderRadius="8"
                 />
             </div>
@@ -138,7 +107,7 @@ definePage('PromotionDetailPage', {
         return {
             title: 'PREMIUM SERVICE PROMOTION FOR WOORI VV LUX POINT',
             time: '01/01/2025 - 31/12/2025',
-            termsLink: '#', // Placeholder for terms and conditions link
+            termsLink: '#',
             promotionInfo: [
                 'Airport lounge tickets apply at the airport lounge system (domestic terminal) operated by Noi Bai Aviation Services Joint Stock Company.',
                 'UrBox electronic voucher worth 2,000,000 VND for use at specified Hotels nationwide.',
@@ -149,11 +118,9 @@ definePage('PromotionDetailPage', {
     },
     template: `
         <div class="bg-gray-100 min-h-screen">
-            <div class="w-full h-48 bg-blue-500 relative">
-                <!-- Placeholder for promotion image -->
-            </div>
+            <div class="w-full h-48 bg-blue-500 relative shadow-custom "></div>
             <div class="p-5 flex flex-col gap-6 relative -mt-20">
-                <div class=" bg-white shadow-md p-4 text-left" style="border-radius: 12px;">
+                <div class="bg-white p-4 text-left shadow-custom rounded-lg">
                     <h1 class="text-md font-bold text-gray-800">{{ title }}</h1>
                     <p class="text-sm text-gray-500 mt-2">Time: {{ time }}</p>
                     <div class="my-4 h-[1px] w-full bg-gray-100"></div>
@@ -162,14 +129,7 @@ definePage('PromotionDetailPage', {
                         <a :href="termsLink" class="text-sm text-blue-500 font-medium">Read now</a>
                     </div>
                 </div>
-                <div class=" bg-white shadow-md p-5 text-start" style="border-radius: 12px;">
-                    <h2 class="text-sm font-bold text-gray-800 mb-3">Promotion information</h2>
-                    <ul class="list-decimal list-inside text-start text-sm text-gray-600 space-y-3 ml-2">
-                        <li v-for="(info, index) in promotionInfo" :key="index">{{ info }}</li>
-                    </ul>
-                </div>
-
-                <div class=" bg-white shadow-md p-5 text-start" style="border-radius: 12px;">
+                <div class="bg-white p-5 text-start shadow-custom rounded-lg">
                     <h2 class="text-sm font-bold text-gray-800 mb-3">Promotion information</h2>
                     <ul class="list-decimal list-inside text-start text-sm text-gray-600 space-y-3 ml-2">
                         <li v-for="(info, index) in promotionInfo" :key="index">{{ info }}</li>
